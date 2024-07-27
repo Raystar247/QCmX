ee8776695ed5877fabc0c6b376a93c369a573eb3
Raystar247 <24star10@gmail.com>
2024-07-27 18:40:39 +0900

    add post function on backend
    usernameを持つデータリクエストを受け付ける処理を追加

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { uid } from 'uid';
 
 const app: Application = express();
 const PORT: number = 3000;
 const FRONTEND_URL: string = "http://localhost:5173";
 
 app.use(cors({ origin: FRONTEND_URL }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 try {
     app.listen(PORT, () => {
@@ -20,4 +23,12 @@ try {
 app.get('/', (req: Request, res: Response) => {
     console.log("getリクエストを受け付けました");
     return res.status(200).json({ message: 'Hello World' });
+});
+
+app.post('/user/register', (req: Request, res: Response) => {
+    console.log('postリクエストを受け付けました');
+    const { username } = req.body.data;
+    console.log(req.body.data.username);
+    const uidValue = uid();
+    return res.status(200).json({ id: uidValue, username });
 });
