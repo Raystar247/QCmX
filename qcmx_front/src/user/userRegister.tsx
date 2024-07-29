4de3eaf1f70ea11d85f3cffd5975de04aa7b0920
Raystar247 <24star10@gmail.com>
2024-07-29 22:30:20 +0900

    Change the repository structure of the folder: 'qcmx_front'
    qcmx_frontフォルダのディレクトリ構成を変更した
    - user: ユーザー登録機能

---
+++ qcmx_front/src/user/userRegister.tsx
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+const BACKEND_URL: string = "http://localhost:3000";
+
+type UserType = {
+  id: string;
+  username: string;
+}
+
+type UserRegisterFormType = {
+  username: string;
+}
+
+const UserRegister: React.FC = () => {
+
+  const { register, handleSubmit } = useForm<UserRegisterFormType>();
+  
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  const addUser = async (event: UserRegisterFormType) => {
+    const { username } = event;
+    console.log(username);
+    await axios.post(`${BACKEND_URL}/user/register`, {
+              data: { username }
+            })
+            .then((response) => {
+              console.log(response.data);
+              const username = response.data;
+              setUsers((_preUsers) => [username, ..._preUsers]);
+            })
+            .catch((error) => { console.log(error); });
+  }
+
+  useEffect(() => {
+    axios.get(BACKEND_URL)
+      .then((response) => {
+        console.log(response.data);
+        const { users } = response.data;
+        setUsers(users);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(addUser)}>
+        <input {...register("username")} type="text" />
+        <button type="submit">add</button>
+      </form>
+      {users.map((user) => (
+        <p key={user.id}>{user.username}</p>
+      ))}
+    </>
+  );
+}
+
+export default UserRegister;
